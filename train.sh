9f1440e6fb0a82dc8a46ef3c99ab9628abecb99b
smuss <smussakhojayeva@gmail.com>
2025-04-10 23:50:16 -0700

    changes

--- train.sh
+++ train.sh
@@ -1,20 +1,5 @@
-#!/bin/sh
-#SBATCH --job-name="gemma3-27b-train"
-#SBATCH --nodes=1
-#SBATCH --partition=defq
-#SBATCH --nodelist=node006
-#SBATCH --cpus-per-task=8
-#SBATCH --output="test-single-node-training-%j.out"
-#SBATCH --gres=gpu:8
-#SBATCH --ntasks-per-node=8
-#SBATCH --time=168:00:00
-# Environment variables for faster initialization
 export OMP_NUM_THREADS=24
 export MKL_NUM_THREADS=1
-
-# Conda initialization
-eval "$(conda shell.bash hook)"
-conda activate axolotl
 
 export WANDB_API_KEY=30bd837ea9ae343e8ccf351903239c04e0cfb3db
 
